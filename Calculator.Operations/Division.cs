099be194b158b4b898623bf318484b99322879a9
DYudin <extreemdv@mail.ru>
2016-07-14 19:01:09 +0500

    inital

---
+++ Calculator.Operations/Division.cs
@@ -0,0 +1,28 @@
+﻿
+using Calculator.Abstract;
+
+namespace Calculator.Operations
+{
+    public class Division : IOperation
+    {
+        public float ExecuteOperation(params float[] parameters)
+        {
+            return 0;
+        }
+
+        public int Priority
+        {
+            get { return 3; }
+        }
+
+        public string Sign
+        {
+            get { return "/"; }
+        }
+
+        public int NumberOfParameters
+        {
+            get { return 2; }
+        }
+    }
+}
